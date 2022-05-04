63c4b2d03f6a8d3c38c1a312d918c50418a2e27b
Erin <erin.steiner@noaa.gov>
2022-05-04 12:35:43 -0700

    Clean up datvars to not reply on data files

--- PerformanceMetrics/external/explorer/explorerSourceFiles/metric_names/datvars.R
+++ PerformanceMetrics/external/explorer/explorerSourceFiles/metric_names/datvars.R
@@ -1,27 +1,22 @@
-load("PerformanceMetrics/data/CVperfmetrics.RData") 
-load("PerformanceMetrics/data/Mperfmetrics.RData") 
-load("PerformanceMetrics/data/CPperfmetrics.RData")
-load("PerformanceMetrics/data/FRperfmetrics.RData") 
+# Variables needed for selectors (Excludes metrics which are listed in metric_names file to allow creation of the html links)
 
 currentyear <- 2020
 currentyearFR <- 2020
 nrcomponents <- c('Revenue', 'Variable costs', 'Fixed costs', 'Variable cost net revenue', 'Total cost net revenue')
 
 # CV 
-  datVars_cv <- with(
-    CVperfmetrics,
-    list(
-      YEAR = 2004:currentyear,
-      NRlist = nrcomponents,
-      CATEGORY = c(
+datVars_cv <- list(
+    YEAR = 2004:currentyear,
+    NRlist = nrcomponents,
+    CATEGORY = c(
         "Fisheries",
         "Homeport",
         "State of homeport" = "State",
         "Vessel length class"
-      ),
-      inclAK = unique(inclAK),
-      whitingv = c("All vessels", "Non-whiting vessels", "Whiting vessels"),
-      STAT =  c(
+    ),
+    inclAK = c("NO", "YES", ""),
+    whitingv = c("All vessels", "Non-whiting vessels", "Whiting vessels"),
+    STAT =  c(
         "Mean per vessel",
         "Mean per vessel/day",
         "Mean per vessel/metric ton caught",
@@ -34,26 +29,23 @@ nrcomponents <- c('Revenue', 'Variable costs', 'Fixed costs', 'Variable cost net
         "Fleet-wide average/day",
         "Fleet-wide average/metric ton caught",
         "Fleet-wide average/dollar revenue"
-      )
     )
-  )
-  
+)
+
 save(datVars_cv, file = "PerformanceMetrics/data/datvars_cv.RData")  
-  
+
 # FR
-  datVars_fr <- with(
-    FRperfmetrics,
-    list(
-      YEAR = 2004:currentyearFR,
-      NRlist = c('Revenue', 'Seafood sales revenue', 'Offload revenue', 'Custom processing and other revenue',
-                 'Variable costs', 'Fixed costs', 'Variable cost net revenue', 'Total cost net revenue'),
-      CATEGORY = c("Production activities" = "Fisheries", "Region", "Processor size"),
-      whitingv = c(
+datVars_fr <- list(
+    YEAR = 2004:currentyearFR,
+    NRlist = c('Revenue', 'Seafood sales revenue', 'Offload revenue', 'Custom processing and other revenue',
+        'Variable costs', 'Fixed costs', 'Variable cost net revenue', 'Total cost net revenue'),
+    CATEGORY = c("Production activities" = "Fisheries", "Region", "Processor size"),
+    whitingv = c(
         "All processors",
         "Whiting processors",
         "Non-whiting processors"
-      ),
-      STAT =  c(
+    ),
+    STAT =  c(
         "Mean per processor",
         "Mean per processor/metric ton produced",
         "Mean per processor/dollar of revenue",
@@ -63,22 +55,19 @@ save(datVars_cv, file = "PerformanceMetrics/data/datvars_cv.RData")
         "Industry-wide total",
         "Industry-wide average/metric ton produced",
         "Industry-wide average/dollar of revenue"
-      )
     )
-  )
+)
 
-  save(datVars_fr, file = "PerformanceMetrics/data/datvars_fr.RData") 
-  
+save(datVars_fr, file = "PerformanceMetrics/data/datvars_fr.RData") 
+
 # MS
-  datVars_ms <- with(
-    Mperfmetrics,
-    list(
-      YEAR = 2004:currentyear,
-      NRlist = nrcomponents,
-      CATEGORY = "Fisheries",
-      inclAK = unique(inclAK),
-      whitingv = "Whiting vessels",
-      STAT =  c(
+datVars_ms <- list(
+    YEAR = 2004:currentyear,
+    NRlist = nrcomponents,
+    CATEGORY = "Fisheries",
+    inclAK = "",
+    whitingv = "Whiting vessels",
+    STAT =  c(
         "Mean per vessel",
         "Mean per vessel/day",
         "Mean per vessel/metric ton purchased",
@@ -94,23 +83,20 @@ save(datVars_cv, file = "PerformanceMetrics/data/datvars_cv.RData")
         'Fleet-wide average/metric ton purchased',
         'Fleet-wide average/metric ton produced',
         'Fleet-wide average/dollar of revenue'
-      )
     )
-  )
-  
-  save(datVars_ms, file = "PerformanceMetrics/data/datvars_ms.RData") 
+)
+
+save(datVars_ms, file = "PerformanceMetrics/data/datvars_ms.RData") 
 
 # CP
-  
-  datVars_cp <- with(
-    CPperfmetrics,
-    list(
-      YEAR = 2004:currentyear,
-      NRlist = nrcomponents,
-      CATEGORY = "Fisheries",
-      inclAK = unique(inclAK),
-      whitingv = "Whiting vessels",
-      STAT =  c(
+
+datVars_cp <- list(
+    YEAR = 2004:currentyear,
+    NRlist = nrcomponents,
+    CATEGORY = "Fisheries",
+    inclAK = "",
+    whitingv = "Whiting vessels",
+    STAT =  c(
         "Mean per vessel",
         "Mean per vessel/day",
         "Mean per vessel/metric ton produced",
@@ -126,7 +112,7 @@ save(datVars_cv, file = "PerformanceMetrics/data/datvars_cv.RData")
         'Fleet-wide average/metric ton produced',
         'Fleet-wide average/metric ton caught',
         'Fleet-wide average/dollar of revenue'
-      )
     )
-  )
-  save(datVars_cp, file = "PerformanceMetrics/data/datvars_cp.RData") 
+)
+
+save(datVars_cp, file = "PerformanceMetrics/data/datvars_cp.RData") 
