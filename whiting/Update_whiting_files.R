1ac23d0edb55f8a6fd6fddbafc224bd2c26267ed
Erin Steiner <erin.steiner@noaa.gov>
2025-02-28 16:50:36 -0800

    Rsconnect file updates

--- whiting/Update_whiting_files.R
+++ whiting/Update_whiting_files.R
@@ -2,7 +2,7 @@
 
 # Display list of available date stamp folders for whiting
 dir("R:/Confidential/FISHEyE/data/Whiting")
-whitingdir <- "R:/Confidential/FISHEyE/data/Whiting/2024-12-09"
+whitingdir <- "R:/Confidential/FISHEyE/data/Whiting/2025-02-27"
 whitingfiles <- list.files(whitingdir)[grepl('RDS', list.files(whitingdir))]
 
 try(if(length(whitingfiles) == 0) stop("There aren't any files to move in that folder"))
