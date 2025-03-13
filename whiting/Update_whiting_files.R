0eb529075250666b8f51950aa06e3ff8677e9988
Amanda Phillips <amanda.phillips@noaa.gov>
2025-03-13 11:44:31 -0700

    fixed typo in update whiting file

--- whiting/Update_whiting_files.R
+++ whiting/Update_whiting_files.R
@@ -2,17 +2,22 @@
 
 # Display list of available date stamp folders for whiting
 dir("R:/Confidential/FISHEyE/data/Whiting")
-whitingdir <- "R:/Confidential/FISHEyE/data/Whiting/2025-02-27"
+whitingdir <- "R:/Confidential/FISHEyE/data/Whiting/2025-03-13"
 whitingfiles <- list.files(whitingdir)[grepl('RDS', list.files(whitingdir))]
 
+file.path(whitingdir, whitingfiles)
+
+getwd()
+
 try(if(length(whitingfiles) == 0) stop("There aren't any files to move in that folder"))
+
 
 # copy all of the whiting files from fisheyedataprep output folder to app folder
 for(wfiles in whitingfiles) {
 
     file.copy(
         from = file.path(whitingdir, whitingfiles),
-        to = "../Whiting",
+        to = "./whiting",
         overwrite = T)
 
 }
