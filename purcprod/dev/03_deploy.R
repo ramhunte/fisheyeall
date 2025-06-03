5d05994fc6221b58c3fd5b6c9e93bbe44f941d4b
erin.steiner-NOAA <erin.steiner@noaa.gov>
2025-06-03 14:27:34 -0700

    Attempt to prep app for deployment to shiny app Updated the list of objects in deployApp() Moved defl.rds Ran manifest command

--- purcprod/dev/03_deploy.R
+++ purcprod/dev/03_deploy.R
@@ -45,15 +45,15 @@ rsconnect::writeManifest()
 ## In command line.
 rsconnect::deployApp(
   appName = desc::desc_get_field("Package"),
-  appTitle = desc::desc_get_field("Package"),
+  appTitle = desc::desc_get_field("Title"),
   appFiles = c(
     # Add any additional files unique to your app here.
     "R/",
     "inst/",
-    "data/",
+    "data-raw/",
     "NAMESPACE",
     "DESCRIPTION",
-    "app.R"
+    "dev/"
   ),
   appId = rsconnect::deployments(".")$appID,
   lint = FALSE,
