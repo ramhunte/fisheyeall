057dde986cb1eb963e8e4c83c56d194899b68aa1
ramhunte <rmhunter999@gmail.com>
2025-06-04 15:41:05 -0700

    starting to migrate data processing from data_processing.R script to targets in data processing repo

--- purcprod/data-raw/data_processing.R
+++ purcprod/data-raw/data_processing.R
@@ -9,79 +9,13 @@
 ########################### Reading  Raw data #################################
 
 # purchase production data
-raw_purcprod <- readRDS("data-raw/mini_purcprod_targ.RDS")
+clean_purcprod <- readRDS("data-raw/mini_purcprod_targ.RDS")
 
 # EDC coverage data
 coverage <- readRDS("data-raw/coverage.rds")
 
-# degp deflator
-load("data-raw/gdp_defl.RData")
-
-########################### Cleaning Raw data #################################
-
-# this df is a cleaned version of the raw data
-clean_purcprod <- raw_purcprod |>
-  dplyr::left_join(gdp_defl, by = c("Year" = "YEAR")) |>
-  # making all variable names lowercase for consistency
-  janitor::clean_names() |>
-  dplyr::filter(statistic == "Total") |>
-  # adjusting raw values by their assigned units
-  # ex: if a value is labeled "millions", then we divide it by 1,000,000 to make it abbreviated
-  # do this for variance, quartiles, value, upper/lower
-  dplyr::mutate(
-    variance = dplyr::case_when(
-      unit == '' ~ variance,
-      unit == 'thousands' ~ variance / 1e3,
-      unit == 'millions' ~ variance / 1e6,
-      unit == 'billions' ~ variance / 1e9,
-      T ~ -999
-    ),
-    q25 = dplyr::case_when(
-      unit == '' ~ q25,
-      unit == 'thousands' ~ q25 / 1e3,
-      unit == 'millions' ~ q25 / 1e6,
-      unit == 'billions' ~ q25 / 1e9,
-      T ~ -999
-    ),
-    q75 = dplyr::case_when(
-      unit == '' ~ q75,
-      unit == 'thousands' ~ q75 / 1e3,
-      unit == 'millions' ~ q75 / 1e6,
-      unit == 'billions' ~ q75 / 1e9,
-      T ~ -999
-    ),
-    value = dplyr::case_when(
-      unit == '' ~ value,
-      unit == 'thousands' ~ value / 1e3,
-      unit == 'millions' ~ value / 1e6,
-      unit == 'billions' ~ value / 1e9,
-      T ~ -999
-    ),
-    upper = dplyr::case_when(
-      statistic == 'Mean' ~ value + variance,
-      statistic == 'Median' ~ q75,
-      statistic == 'Total' ~ value
-    ),
-    lower = dplyr::case_when(
-      statistic == 'Mean' ~ value - variance,
-      statistic == 'Median' ~ q25,
-      statistic == 'Total' ~ value
-    )
-  ) |>
-  dplyr::mutate(
-    # new labels for plot facets
-    unit_lab = stringr::str_replace(
-      ylab,
-      "(?<=:)(.*)",
-      # removing statistic
-      ~ stringr::str_remove_all(.x, "(Mean|Median|Total|[()])")
-    ) |>
-      # removing extra blank space
-      stringr::str_squish()
-  ) |>
-  data.frame()
-
 ####################### Cleaning "Summary" tab data ###########################
+
 # this section subsets the data to be used for the "Summary" tab on the "Explore the Data" page
 
 # subsetting data for the "Production Activities" bottom tab under "Summary"
@@ -110,32 +44,36 @@ sumdf_size <- clean_purcprod |>
 # this section subsets the data to be used for the "By Product Type" tab on the "Explore the Data" page
 
 # cleaning data for the "By Product Type" tab
-proddf <- dplyr::filter(clean_purcprod, tab == 'Product') |> # production tab data
-  tidyr::separate(
-    metric,
-    into = c("type", "metric"),
-    sep = " (?=\\()",
-    extra = "merge"
-  ) |>
-  dplyr::mutate(metric = substr(metric, 2, nchar(metric) - 1)) |>
-  dplyr::filter(!is.na(value))
+# proddf <- dplyr::filter(clean_purcprod, tab == 'Product') |> # production tab data
+  # tidyr::separate(
+  #   metric,
+  #   into = c("type", "metric"),
+  #   sep = " (?=\\()",
+  #   extra = "merge"
+  # ) |>
+  # dplyr::mutate(metric = substr(metric, 2, nchar(metric) - 1)) |>
+  # dplyr::filter(!is.na(value))
 
 # subsetting data for the "Production Activities" bottom tab under "By Product Type"
-proddf_prac <- proddf |>
-  dplyr::filter(cs == "")
+proddf_prac <- clean_purcprod |>
+  dplyr::filter(tab == 'Product',
+                cs == "",
+                !is.na(value))
 
 # subsetting data for the "Region" bottom tab under "By Product Type"
-proddf_reg <- proddf |>
+proddf_reg <- clean_purcprod |>
   dplyr::filter(
+    tab == 'Product',
     cs != "",
     variable %in% c("California", "Washington and Oregon")
   )
 
 # subsetting data for the "Processor size/type" bottom tab under "By Product Type"
-proddf_size <- proddf |>
+proddf_size <- clean_purcprod |>
   dplyr::filter(
-    # cs != "",
-    variable %in% c("Small", "Medium", "Large", "Non-processor")
+    tab == 'Product',
+    variable %in% c("Small", "Medium", "Large", "Non-processor"),
+    !is.na(value)
   )
 
 
@@ -146,55 +84,24 @@ proddf_size <- proddf |>
 # here (unlike in the previous data frames), so I am restructuring raw data so it is
 # all shown as numbers in the millions and not also thousands.
 
-specsdf <- raw_purcprod |>
-  dplyr::left_join(gdp_defl, by = c("Year" = "YEAR")) |>
-  dplyr::select(-ylab) |>
-  janitor::clean_names() |>
-  dplyr::filter(
-    tab == 'Product', # production tab data
-    cs == "",
-    statistic == "Total",
-    !is.na(value)
-  ) |>
-  tidyr::separate(
-    metric,
-    into = c("type", "metric"),
-    sep = " (?=\\()",
-    extra = "merge"
-  ) |>
-  dplyr::mutate(
-    metric = substr(metric, 2, nchar(metric) - 1)
-    # unit_lab = paste0(variable, " (", metric, "): ", unit, " nominal $")
-  ) |>
-  dplyr::select(-c(variance, q25, q75)) |>
+specsdf <- proddf_prac %>%
 
   # original data filter already classified labels as millions, billions,
-  dplyr::mutate(
-    unit = dplyr::case_when(
-      metric == "Production weight" ~ "millions",
-      metric == "Production value" ~ "millions",
-      metric == "Production price (per lb)" ~ ""
-    ),
-    value = dplyr::case_when(
-      metric == "Production weight" ~ value / 1e6,
-      metric == "Production value" ~ value / 1e6,
-      metric == "Production price (per lb)" ~ value
-    )
-  ) |>
+  # dplyr::mutate(
+  #   unit = dplyr::case_when(
+  #     metric == "Production weight" ~ "millions",
+  #     metric == "Production value" ~ "millions",
+  #     metric == "Production price (per lb)" ~ ""
+  #   ),
+  #   value = dplyr::case_when(
+  #     metric == "Production weight" ~ value / 1e6,
+  #     metric == "Production value" ~ value / 1e6,
+  #     metric == "Production price (per lb)" ~ value
+  #   )
+  # ) |>
   rbind(
     clean_purcprod |>
-      dplyr::select(-c(ylab, upper, lower, unit_lab, variance, q25, q75)) |>
-      dplyr::filter(tab == "Overview") |>
-      tidyr::separate(
-        metric,
-        into = c("type", "metric"),
-        sep = " (?=\\()",
-        extra = "merge"
-      ) |>
-      dplyr::mutate(
-        metric = substr(metric, 2, nchar(metric) - 1)
-        # unit_lab = paste0(variable, " (", metric, "): ", unit, " nominal $")
-      )
+      dplyr::filter(tab == "Overview")
   )
 
 
@@ -217,14 +124,6 @@ overviewdf <- clean_purcprod |>
   dplyr::filter(
     tab == "Overview"
   ) |>
-  tidyr::separate(
-    metric,
-    into = c("type", "metric"),
-    sep = " (?=\\()",
-    extra = "merge"
-  ) |>
-  dplyr::mutate(metric = substr(metric, 2, nchar(metric) - 1)) |>
-
   # adding in "All" option from summary data frame
   rbind(
     sumdf_prac |>
@@ -234,7 +133,6 @@ overviewdf <- clean_purcprod |>
       ) |>
       dplyr::mutate(type = "All", tab = "Overview")
   ) |>
-  dplyr::select(-c(cs, upper, lower, variance, q25, q75)) |>
   dplyr::filter(
     metric %in% c("Production value", "Production weight")
   ) |>
